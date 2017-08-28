1daa7686abb5fd57fa55e43636f539a3f05b7d85
Martishevich Oleg <oleg.martishevich@gmail.com>
2017-08-29 00:07:58 +0300

    #7 state of components

--- src/app/index.js
+++ src/app/index.js
@@ -3,12 +3,9 @@ import { render } from "react-dom";
 
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
+
 class App extends React.Component {
   render() {
-    var user = {
-      name: "Anna",
-      hobbies: ["Sports", "Reading"]
-    };
     return (
         <div className="container">
           <div className="row">
@@ -18,9 +15,7 @@ class App extends React.Component {
           </div>
           <div className="row">
             <div className="col-xs-10 col-xs-offset-1">
-              <Home name={"Max"} age={27} user={user}>
-                <p>This is a paragraph</p>
-              </Home>
+              <Home name={"Max"} age={27}/>
             </div>
           </div>
         </div>
