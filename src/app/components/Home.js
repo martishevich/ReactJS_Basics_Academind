b7aaf87ca99bb59afb86054e99b7807df4987c2d
Martishevich Oleg <oleg.martishevich@gmail.com>
2017-08-29 00:16:19 +0300

    #8 state of components

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,20 +5,22 @@ export class Home extends React.Component {
 
   constructor(props) {
     super();
-    this.age = props.age;
-
+    this.state = {
+      age: props.initialAge
+    }
   }
 
   onMakeOlder() {
-    this.age += 3;
-    console.log(this.age);
+    this.setState({
+      age: this.state.age + 3
+    });
   }
 
   render() {
     return (
         <div>
           <p>In a new Component!</p>
-          <p>Your name is {this.props.name}, your age is {this.props.age}</p>
+          <p>Your name is {this.props.name}, your age is {this.state.age}</p>
           <hr/>
           <button onClick={() => this.onMakeOlder()} className="btn = btn-primary">Make me older!</button>
         </div>
@@ -28,5 +30,5 @@ export class Home extends React.Component {
 
 Home.propTypes = {
   name: React.PropTypes.string,
-  age: React.PropTypes.number,
+  initialAge: React.PropTypes.number,
 }
